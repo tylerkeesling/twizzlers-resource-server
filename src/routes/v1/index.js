d2e17f33c5cfa08ac43c1c0f6d05280339daa6b6
Tyler <tyler.keesling@fitchsolutions.com>
2022-01-02 15:18:04 -0500

    api token

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,17 +1,14 @@
 var express = require('express');
-const testRoute = require('./test.route');
 const usersRoute = require('./users.route');
 const groupsRoute = require('./groups.route');
 
 var router = express.Router();
 
 const defaultRoutes = [
-  { path: '/test', route: testRoute },
   { path: '/users', route: usersRoute },
   { path: '/groups', route: groupsRoute },
 ];
 
-// hello
 defaultRoutes.forEach((route) => {
   router.use(route.path, route.route);
 });
